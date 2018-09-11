d5502451a9ecacb533374e49a04af1d8afcba0fb
cory_puckett <cory_puckett@student.wayland.k12.ma.us>
2018-09-11 09:05:56 -0400

    added cells in constructor

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
 public class Main {
+    /*format for in
+    string[] arr = line.split(" "); splits the string at spaces
+     */
     public static void main(String[] args) {
 
     }
--- src/Solver.java
+++ src/Solver.java
@@ -3,8 +3,14 @@
 
 public class Solver {
     private int[][] startconfig; // strating configuration of the puzzle
+    private Cell[][] cells;
 
     public Solver(int[][] startconfig) {
         this.startconfig = startconfig;
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+                cells[r][c] = new Cell(startconfig[r][c]);
+            }
+        }
     }
 }
