2eb416855713b3f007e32f6ffb52376bbcacc3cf
cory_puckett <cory_puckett@student.wayland.k12.ma.us>
2018-09-14 10:12:34 -0400

    Made reader file work

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
     /*format for in
     string[] arr = line.split(" "); splits the string at spaces
      */
     public static void main(String[] args) {
-
+        Reader file = new Reader(1);
 
     }
 }
--- src/Reader.java
+++ src/Reader.java
@@ -1,5 +1,39 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Reader {
     private int[][] ans;//the read file number array
+    private int filenum;//file number
 
+    public Reader(int filenum) {
+        ans = new int[9][9];
+        this.filenum = filenum;
+        try {
+            File file = new File("./res/s0" + filenum + "a.txt");
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line;
+            int r = 0;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] row = line.split(" ");
 
+                if(r == 9)
+                    break;
+                for (int c = 0; c < row.length; c++) {
+                    ans[r][c] = Integer.parseInt(row[c]);
+                }
+                r++;
+            }
+            fileReader.close();
+
+        } catch (IOException e) {
+            System.out.println("Error");
+        }
+    }
+
+    public int[][] getAns() {
+        return ans;
+    }
 }
